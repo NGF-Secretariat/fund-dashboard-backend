6fd7678e570e5ea0d3a502249fe160c49b878942
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2026-04-21 15:08:40 +0100

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Fund dashboard backend is running!';
   }
 }
