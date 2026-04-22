10e33584da126e7cf7661722580f475352a769fd
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2026-04-22 16:49:08 +0100

    test robot

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health')
+  health() {
+    return { status: 'ok', timestamp: new Date() };
+  }
+
 }
